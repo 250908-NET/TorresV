d40a83018bee1ea5ace074815c709136775631bf
Victor Torres <vitugo7@hotmail.com>
2025-09-18 10:56:35 -0400

    commit

--- Week1/ToDoApp/Program.cs
+++ Week1/ToDoApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Serilog;
+
+namespace ToDOApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         var builder = WebApplication.CreateBuilder(args);
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+        builder.Host.UseSerilog();    
+
+var app = builder.Build();
+app.MapGet("/", (ILogger) =>
+{
+    logger.LogInformation("Hello, World! endpoint was called");
+    return "Hello World!";
+}); 
+        }
+    }
+}
+
+  
+
