0ffe02faffe97f11e2555ec07c520a527ef9e234
vitugo23 <vitugo7@hotmail.com>
2025-09-24 17:05:20 -0400

    Challenge8 done

--- Week1/CodingChallenges/8_Loops/8_Loops/Program.cs
+++ Week1/CodingChallenges/8_Loops/8_Loops/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace _8_LoopsChallenge
@@ -9,29 +7,54 @@ namespace _8_LoopsChallenge
     {
         public static void Main(string[] args)
         {
-            /* Your code here */
-
+            // You can add manual tests here if you want
+            // but unit tests should drive the verification.
         }
 
         /// <summary>
-        /// Return the number of elements in the List<int> that are odd.
+        /// Return the number of elements in the List<int> that are even (excluding 1234).
         /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
         public static int UseFor(List<int> x)
         {
-            throw new NotImplementedException("UseFor() is not implemented yet.");
+            int count = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                if (x[i] != 1234 && x[i] % 2 == 0) 
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         /// <summary>
         /// This method counts the even entries from the provided List<object> 
         /// and returns the total number found.
         /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
         public static int UseForEach(List<object> x)
         {
-            throw new NotImplementedException("UseForEach() is not implemented yet.");
+            int count = 0;
+            foreach (var item in x)
+            {
+                if (item is char) continue;
+
+                if (item is IConvertible conv)
+                {
+                    try
+                    {
+                        long val = conv.ToInt64(null);
+                        if (val % 2 == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+            return count;
         }
 
         /// <summary>
@@ -39,101 +62,60 @@ namespace _8_LoopsChallenge
         /// Exit the loop when the integer 1234 is found.
         /// Return the total number of multiples of 4.
         /// </summary>
-        /// <param name="x"></param>
         public static int UseWhile(List<int> x)
         {
-            throw new NotImplementedException("UseFor() is not implemented yet.");
+            int count = 0;
+            int i = 0;
+
+            while (i < x.Count)
+            {
+                if (x[i] == 1234)
+                {
+                    break;
+                }
+                if (x[i] % 4 == 0)
+                {
+                    count++;
+                }
+                i++;
+            }
+            return count;
         }
 
         /// <summary>
         /// This method will evaluate the Int Array provided and return how many of its 
         /// values are multiples of 3 and 4.
         /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
         public static int UseForThreeFour(int[] x)
         {
-            throw new NotImplementedException("UseForThreeFour() is not implemented yet.");
+            int count = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 3 == 0 && x[i] % 4 == 0) 
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         /// <summary>
         /// This method takes an array of List<string>'s. 
         /// It concatenates all the strings, with a space between each, in the Lists and returns the resulting string.
         /// </summary>
-        /// <param name="stringListArray"></param>
-        /// <returns></returns>
         public static string LoopdyLoop(List<string>[] stringListArray)
         {
-            throw new NotImplementedException("LoopdyLoop() is not implemented yet.");
+            List<string> allStrings = new List<string>();
+
+            foreach (var list in stringListArray)
+            {
+                foreach (var str in list)
+                {
+                    allStrings.Add(str);
+                }
+            }
+
+            return string.Join(" ", allStrings) + " ";
         }
     }
 }
-
-//using System;
-//using System.Collections.Generic;
-
-//namespace _8_LoopsChallenge
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            /* Your code here */
-
-//        }
-
-//        /// <summary>
-//        /// Return the number of elements in the List<int> that are odd.
-//        /// </summary>
-//        /// <param name="x"></param>
-//        /// <returns></returns>
-//        public static int UseFor(List<int> x)
-//        {
-//            throw new NotImplementedException("UseFor() is not implemented yet.");
-//        }
-
-//        /// <summary>
-//        /// This method counts the even entries from the provided List<object> 
-//        /// and returns the total number found.
-//        /// </summary>
-//        /// <param name="x"></param>
-//        /// <returns></returns>
-//        public static int UseForEach(List<object> x)
-//        {
-//            throw new NotImplementedException("UseForEach() is not implemented yet.");
-//        }
-
-//        /// <summary>
-//        /// This method counts the multiples of 4 from the provided List<int>. 
-//        /// Exit the loop when the integer 1234 is found.
-//        /// Return the total number of multiples of 4.
-//        /// </summary>
-//        /// <param name="x"></param>
-//        public static int UseWhile(List<int> x)
-//        {
-//            throw new NotImplementedException("UseFor() is not implemented yet.");
-//        }
-
-//        /// <summary>
-//        /// This method will evaluate the Int Array provided and return how many of its 
-//        /// values are multiples of 3 and 4.
-//        /// </summary>
-//        /// <param name="x"></param>
-//        /// <returns></returns>
-//        public static int UseForThreeFour(int[] x)
-//        {
-//            throw new NotImplementedException("UseForThreeFour() is not implemented yet.");
-//        }
-
-//        /// <summary>
-//        /// This method takes an array of List<string>'s. 
-//        /// It concatenates all the strings, with a space between each, in the Lists and returns the resulting string.
-//        /// </summary>
-//        /// <param name="stringListArray"></param>
-//        /// <returns></returns>
-//        public static string LoopdyLoop(List<string>[] stringListArray)
-//        {
-//            throw new NotImplementedException("LoopdyLoop() is not implemented yet.");
-//        }
-//    }
-//}
