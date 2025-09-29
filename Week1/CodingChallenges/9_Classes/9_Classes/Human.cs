5e4a14e69e3b31534278b2795e47512fa4a67b0d
vitugo23 <vitugo7@hotmail.com>
2025-09-29 17:01:43 -0400

    challenge9 done

--- Week1/CodingChallenges/9_Classes/9_Classes/Human.cs
+++ Week1/CodingChallenges/9_Classes/9_Classes/Human.cs
@@ -6,5 +6,24 @@ namespace _9_ClassesChallenge
 {
     internal class Human
     {
-    }//end of class
-}//end of namespace
+        private string firstName = "Pat";
+        private string lastName = "Smyth";
+
+        // Parameterless constructor
+        public Human() { }
+
+        // Parameterized constructor
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // Method
+        public string AboutMe()
+        {
+            return $"My name is {firstName} {lastName}.";
+        } // end of class
+    } //end of namespace
+
+}
