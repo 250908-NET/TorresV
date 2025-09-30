271cbc17c7ba07aae93e442a41b1d3f24213fc67
vitugo23 <vitugo7@hotmail.com>
2025-09-29 21:49:10 -0400

    API project

---
+++ API_Project/ControllerTutorial.API/Controllers/courseController.cs
@@ -0,0 +1,13 @@
+using Microsofrt.AspNetCore.Mvc;
+
+namespace School.Controllers
+{
+    public class StudentsController : Controller BadRequest
+    {
+        // fields
+
+        //methods
+
+        // constructor
+    }
+}
